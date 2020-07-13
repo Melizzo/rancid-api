9ba7414a5d96f8ee55907eb38a21434ce08eec00
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-13 15:59:52 -0600

    :rocket: adding nodemon:

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+
+app.set('port', process.env.PORT || 3002)
+app.locals.title='Rancid API';
+
+app.get('/', (request, response) => {
+  response.send('Hello there')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`app is running port ${app.get("port")}`);
+})
+
+
